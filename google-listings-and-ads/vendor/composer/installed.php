4ab667792a33d7113ab02c842b7b5e5c5dbf710f
Justin Gardner <justin@rhynorater.com>
2024-06-26 06:02:08 -0500

    2.7.5

--- google-listings-and-ads/vendor/composer/installed.php
+++ google-listings-and-ads/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/google-listings-and-ads',
-        'pretty_version' => 'dev-release/2.7.3',
-        'version' => 'dev-release/2.7.3',
-        'reference' => 'c0ddf1f6fcb02087f3277033afc67cd2cb9b82ee',
+        'pretty_version' => 'dev-release/2.7.5',
+        'version' => 'dev-release/2.7.5',
+        'reference' => '726e9b2d83faca4c19fd8c9d64f716912c8bb57f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -455,9 +455,9 @@
             'dev_requirement' => false,
         ),
         'woocommerce/google-listings-and-ads' => array(
-            'pretty_version' => 'dev-release/2.7.3',
-            'version' => 'dev-release/2.7.3',
-            'reference' => 'c0ddf1f6fcb02087f3277033afc67cd2cb9b82ee',
+            'pretty_version' => 'dev-release/2.7.5',
+            'version' => 'dev-release/2.7.5',
+            'reference' => '726e9b2d83faca4c19fd8c9d64f716912c8bb57f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
